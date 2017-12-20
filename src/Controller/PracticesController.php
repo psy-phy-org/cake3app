6294929372b685c56a4d3c39edffedaaed070e52
psy-phy-owner <psyphyowner@gmail.com>
2017-12-20 11:19:31 +0900

    コントローラとビュー - レイアウトの作成と利用（アクションごとにレイアウト動作を変える）

--- src/Controller/PracticesController.php
+++ src/Controller/PracticesController.php
@@ -3,10 +3,22 @@ namespace App\Controller;
 
 class PracticesController extends AppController
 {
-    public $name = 'Practices';
-    public $autoRender = true;
+    public function initialize()
+    {
+        $this->name = 'Practices';
+        $this->autoRender = false;
+        $this->viewBuilder()->autoLayout(false);
+    }
+
     public function index()
     {
+        $this->viewBuilder()->autoLayout(true);
+        $this->autoRender = true;
+    }
+
+    public function other()
+    {
         $this->viewBuilder()->autoLayout(false);
+        $this->autoRender = true;
     }
 }
