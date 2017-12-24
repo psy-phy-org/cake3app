09e59eff13c12a51dfc9adaf9f7b060b4e599f4e
psy-phy-owner <psyphyowner@gmail.com>
2017-12-24 14:39:04 +0900

    データベースの基本的な操作 - レコードの削除

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use \Exception;
+use Cake\Log\Log;
 
 class ArticlesController extends AppController
 {
@@ -16,5 +18,18 @@ class ArticlesController extends AppController
         }
         $this->set('data', $data->toArray());
         $this->set('count', $data->count());
+    }
+
+    public function delRecord()
+    {
+        if ($this->request->is('post')) {
+            try {
+                $entity = $this->Articles->get($this->request->data['id']);
+                $this->Articles->delete($entity);
+            } catch (Exception $e) {
+                Log::write('debug', $e->getMessage());
+            }
+            return $this->redirect(['action' => 'index']);
+        }
     }
 }
--- src/Template/Articles/index.ctp
+++ src/Template/Articles/index.ctp
@@ -1,5 +1,5 @@
 <h1>Articles</h1>
-<?= $this->Form->create($entity, ['url' => ['action' => 'index']]) ?>
+<?= $this->Form->create($entity, ['url' => ['action' => 'delRecord']]) ?>
 <fieldset>
 <?= $this->Form->label('id', 'ID') ?>
 <?= $this->Form->text('id') ?>
