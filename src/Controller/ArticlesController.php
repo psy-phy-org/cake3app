5b420a685d3d47d23cd42bc799b7a1a214212d71
psy-phy-owner <psyphyowner@gmail.com>
2017-12-24 14:48:14 +0900

    データベースの基本的な操作 - レコードの更新

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,28 +2,37 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use \Exception;
+use Cake\Log\Log;
 
 class ArticlesController extends AppController
 {
-    public function index()
+    public function index($id = null)
     {
         $this->set('entity', $this->Articles->newEntity());
-        if ($this->request->is('post')) {
-            $id = $this->request->data['id'];
-            $data = $this->Articles->findByIdOrName($id, $id);
-        } else {
-            $data = $this->Articles->find('all');
+        if ($id != null) {
+            try {
+                $entity = $this->Articles->get($id);
+                $this->set('entity', $entity);
+            } catch (Exception $e) {
+                Log:write('debug', $e->getMessage());
+            }
         }
+        $data = $this->Articles->find('all')->order(['id' => 'DESC']);
         $this->set('data', $data->toArray());
         $this->set('count', $data->count());
     }
 
-    public function delRecord()
+    public function editRecord()
     {
-        if ($this->request->is('post')) {
-            $this->Articles->deleteAll(
-                ['name' => $this->request->data['name']]
-            );
+        if ($this->request->is('put')) {
+            try {
+                $entity = $this->Articles->get($this->request->data['id']);
+                $this->Articles->patchEntity($entity, $this->request->data);
+                $this->Articles->save($entity);
+            } catch (Exception $e) {
+                Log:write('debug', $e->getMessage());
+            }
         }
         return $this->redirect(['action' => 'index']);
     }
