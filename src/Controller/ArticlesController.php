865c6db5e5f53f433c84f5741d7f7ba45305371a
psy-phy-owner <psyphyowner@gmail.com>
2017-12-24 19:09:11 +0900

    QueryBuilderとConnectionManager - 検索条件を設定する Where

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -8,6 +8,12 @@ class ArticlesController extends AppController
     public function index($id = null)
     {
         $data = $this->Articles->find();
+        if ($this->request->is('post')) {
+            $input = $this->request->data['input'];
+            $data = $this->Articles
+                ->find()
+                ->where(['name' => $input]);
+        }
         $this->set('data', $data);
         $this->set('entity', $this->Articles->newEntity());
     }
