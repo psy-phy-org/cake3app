57ea238bd32279ae09adb14a633713e3d230dddc
psy-phy-owner <psyphyowner@gmail.com>
2017-12-25 19:50:30 +0900

    コンポーネント - Flashコンポーネントの使い方

--- src/Controller/PracticesController.php
+++ src/Controller/PracticesController.php
@@ -6,19 +6,11 @@ class PracticesController extends AppController
     public function initialize()
     {
         $this->name = 'Practices';
-        $this->viewBuilder()->Layout('Practices');
-        $this->set('msg', 'Practices/index');
-        $this->set('footer', 'Practices\footer');
+        $this->loadComponent('Flash');
     }
 
     public function index()
     {
-        $result = '';
-        if ($this->request->is('post')) {
-            $result = $this->request->data['PracticesForm']['date'];
-        } else {
-            $result ='日時を選択して送信してください。';
-        }
-        $this->set('result', h($result));
+        $this->Flash->set('クリックすると消えます。');
     }
 }
--- src/Template/Practices/index.ctp
+++ src/Template/Practices/index.ctp
@@ -1,9 +1,2 @@
 <h1>Practice</h1>
 <p>This is a practice of CakePHP.</p>
-<p>
-<?php
-$w = '3 years';
-$d = '2015-1-1';
-echo $this->Time->wasWithinLast($w,$d) ? '今日は'.$d.'から'.$w.'以内です。' : '範囲外！'
-?>
-</p>
