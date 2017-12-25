6670dbef2f9131fb68f9e4b7b8d4ea36a50f5e4e
psy-phy-owner <psyphyowner@gmail.com>
2017-12-25 09:04:44 +0900

    バリデーションの利用 - エラーメッセージを用意する

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -8,10 +8,26 @@ class ArticlesTable extends Table
 {
     public function validationDefault(Validator $validator)
     {
-        $validator->integer('id');
-        $validator->notEmpty('name');
-        $validator->notEmpty('title');
-        $validator->notEmpty('content');
+        $validator
+            ->integer('id');
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name', '必須項目です。')
+            ->add('name', [
+                'minLength' => [
+                    'rule' => ['minLength', 5],
+                    'message' => '5文字以上で入力してください',
+                    'last' => true
+                ],
+                'maxLength' => [
+                    'rule' => ['maxLength', 10],
+                    'message' => '10文字以内で入力してください。'
+                ]
+            ]);
+        $validator
+            ->notEmpty('title');
+        $validator
+            ->notEmpty('content');
         return $validator;
     }
 }
--- src/Template/Articles/add_record.ctp
+++ src/Template/Articles/add_record.ctp
@@ -19,28 +19,3 @@
 </fieldset>
 <?= $this->Form->button('送信') ?>
 <?= $this->Form->end() ?>
-
-<hr>
-
-<table>
-<thead>
-  <tr>
-    <th>ID</th>
-    <th>NAME</th>
-    <th>TITLE</th>
-    <th>CONTENT</th>
-  </tr>
-</thead>
-<tbody>
-<?php
-$arr = $data->toArray();
-for ($i=0; $i<count($arr); $i++) {
-    echo $this->Html->tableCells(
-        $arr[$i]->toArray(),
-        ['style' => 'background-color: #f0f0f0'],
-        true
-    );
-}
-?>
-</tbody>
-</table>
