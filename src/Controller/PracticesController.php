ab4bd2752cbcbb8ea7451e79a0bc56c1286fbed4
psy-phy-owner <psyphyowner@gmail.com>
2017-12-26 13:52:02 +0900

    コンポーネント - CSRFコンポーネント

--- src/Controller/PracticesController.php
+++ src/Controller/PracticesController.php
@@ -1,22 +1,27 @@
 <?php
 namespace App\Controller;
 
+use Cake\Network\Exception\InvalidCsrfTokenException;
+
 class PracticesController extends AppController
 {
     public function initialize()
     {
         $this->name = 'Practices';
         $this->loadComponent('Flash');
+        $this->loadComponent('Csrf');
     }
 
     public function index()
     {
-        $this->Flash->set(
-            'メッセージを表示します。',
-            [
-                'element'=>'info',
-                'key'=>'info'
-            ]
-        );
+        if ($this->request->is('post')) {
+            if (!empty($this->request->data['name']) && !empty($this->request->data['password'])) {
+                $this->Flash->success('OK');
+            } else {
+                $this->Flash->error('bad...');
+            }
+        } else {
+            $this->Flash->info('Please input form:');
+        }
     }
 }
--- src/Template/Practices/index.ctp
+++ src/Template/Practices/index.ctp
@@ -1,3 +1,9 @@
 <h1>Practice</h1>
 <p>This is a practice of CakePHP.</p>
-<div><?= $this->Flash->render('info') ?></div>
+<?= $this->Form->create() ?>
+<fieldset>
+  <?= $this->Form->text('name') ?>
+  <?= $this->Form->password('password') ?>
+</fieldset>
+<?= $this->FOrm->button('送信') ?>
+<?= $this->Form->end() ?>
